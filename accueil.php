790ad10b67754133b63f76d0f1846db5f47d7841
Thibaut <thibaut.denicolas38@gmail.com>
2018-10-24 16:39:03 +0200

    ajout devoir

--- accueil.php
+++ accueil.php
@@ -92,9 +92,9 @@
             <span>Groupes</span>
           </a>
           <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-            <a class="dropdown-item" href="login.php">Groupe S4D</a>
+            <button id="buttonDevoir" class="btn btn-default btn-block" onclick="showDevoir('listeDevoir')">S4D</button>
             <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="404.php">Groupe LV3</a>
+            <button id="buttonDevoir" class="btn btn-default btn-block" onclick="showDevoir('listeDevoir')">LV3</button>
           </div>
         </li>
         <li class="nav-item">
@@ -140,83 +140,69 @@
 
           <div id="zone_affichage">
 
+            <div id="listeDevoir" class="invisible">
+              <div class="container">
+                <h2>Nom du groupe</h2>
 
-          </div>
+                <div class="column">
+                  <div class="col-xl-3 col-sm-6 mb-3">
+                    <div class="card text-white bg-primary o-hidden h-100">
+                      <div class="card-body">
+                        <div class="card-body-icon">
+                          <i class="fas fa-mobile-alt"></i>
+                        </div>
+                        <div class="mr-5">DS d'IHM</div>
+                        <div class="mr-5">15/11/2018</div>
+                      </div>
+                      <a class="card-footer text-white clearfix small z-1" href="#">
+                        <span class="float-left">Voir plus</span>
+                        <span class="float-right">
+                          <i class="fas fa-angle-right"></i>
+                        </span>
+                      </a>
+                    </div>
+                  </div>
+                  <div class="col-xl-3 col-sm-6 mb-3">
+                    <div class="card text-white bg-primary o-hidden h-100">
+                      <div class="card-body">
+                        <div class="card-body-icon">
+                          <i class="fas fa-calculator"></i>
+                        </div>
+                        <div class="mr-5">DS de math</div>
+                        <div class="mr-5">19/11/2018</div>
+                      </div>
+                      <a class="card-footer text-white clearfix small z-1" href="#">
+                        <span class="float-left">Voir plus</span>
+                        <span class="float-right">
+                          <i class="fas fa-angle-right"></i>
+                        </span>
+                      </a>
+                    </div>
+                  </div>
+                  <div class="col-xl-3 col-sm-6 mb-3">
+                    <div class="card text-white bg-primary o-hidden h-100">
+                      <div class="card-body">
+                        <div class="card-body-icon">
+                          <i class="fab fa-java"></i>
+                        </div>
+                        <div class="mr-5">Examen JavaScript</div>
+                        <div class="mr-5">21/11/2018</div>
+                      </div>
+                      <a class="card-footer text-white clearfix small z-1" href="#">
+                        <span class="float-left">Voir plus</span>
+                        <span class="float-right">
+                          <i class="fas fa-angle-right"></i>
+                        </span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
 
-          <!-- Breadcrumbs-->
-          <!--<ol class="breadcrumb">
-            <li class="breadcrumb-item">
-              <a href="#">Dashboard</a>
-            </li>
-            <li class="breadcrumb-item active">Overview</li>
-          </ol>-->
 
-          <!-- Icon Cards-->
-          <div class="column">
-            <div class="col-xl-3 col-sm-6 mb-3">
-              <div class="card text-white bg-primary o-hidden h-100">
-                <div class="card-body">
-                  <div class="card-body-icon">
-                    <i class="fas fa-fw fa-comments"></i>
-                  </div>
-                  <div class="mr-5">26 New Messages!</div>
-                </div>
-                <a class="card-footer text-white clearfix small z-1" href="#">
-                  <span class="float-left">View Details</span>
-                  <span class="float-right">
-                    <i class="fas fa-angle-right"></i>
-                  </span>
-                </a>
-              </div>
-            </div>
-            <div class="col-xl-3 col-sm-6 mb-3">
-              <div class="card text-white bg-warning o-hidden h-100">
-                <div class="card-body">
-                  <div class="card-body-icon">
-                    <i class="fas fa-fw fa-list"></i>
-                  </div>
-                  <div class="mr-5">11 New Tasks!</div>
-                </div>
-                <a class="card-footer text-white clearfix small z-1" href="#">
-                  <span class="float-left">View Details</span>
-                  <span class="float-right">
-                    <i class="fas fa-angle-right"></i>
-                  </span>
-                </a>
-              </div>
-            </div>
-            <div class="col-xl-3 col-sm-6 mb-3">
-              <div class="card text-white bg-success o-hidden h-100">
-                <div class="card-body">
-                  <div class="card-body-icon">
-                    <i class="fas fa-fw fa-shopping-cart"></i>
-                  </div>
-                  <div class="mr-5">123 New Orders!</div>
-                </div>
-                <a class="card-footer text-white clearfix small z-1" href="#">
-                  <span class="float-left">View Details</span>
-                  <span class="float-right">
-                    <i class="fas fa-angle-right"></i>
-                  </span>
-                </a>
-              </div>
-            </div>
-            <div class="col-xl-3 col-sm-6 mb-3">
-              <div class="card text-white bg-danger o-hidden h-100">
-                <div class="card-body">
-                  <div class="card-body-icon">
-                    <i class="fas fa-fw fa-life-ring"></i>
-                  </div>
-                  <div class="mr-5">13 New Tickets!</div>
-                </div>
-                <a class="card-footer text-white clearfix small z-1" href="#">
-                  <span class="float-left">View Details</span>
-                  <span class="float-right">
-                    <i class="fas fa-angle-right"></i>
-                  </span>
-                </a>
-              </div>
-            </div>
+
+
           </div>
 
         <!-- Sticky Footer -->
@@ -256,7 +242,7 @@
           </div>
         </div>
       </div>
-    </div>
+    </div>   
 
     <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
