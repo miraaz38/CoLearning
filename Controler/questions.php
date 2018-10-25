0dcb8f115150c96df46365bb1d2c9191ba0a42ca
CARRIER Damien <carrierd@hostname.iut2.upmf-grenoble.fr>
2018-10-25 11:35:46 +0200

    push dam

--- Controler/questions.php
+++ Controler/questions.php
@@ -3,8 +3,32 @@
 session_start();
 include_once("../Model/colearningDAO_class.php");
 
-if (isset($_GET['id']) AND !empty($_GET['id'])) {
-    $getIdForum = $_GET['id']; // récupère l'id du forum
-}
-
+$idForum = $_GET['id'];
+$questions = $dao-> getQuestionsForum($idForum);
 ?>
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div>
+      <p>FAQ</p>
+      <br>
+      <?php
+
+      while ($c = $questions->fetch()) {
+         $id = $c['idQuestionF'];
+          $pseudo = $dao->getUserById($c['auteurQuestionF']);
+          $dateCom = $c['dateQuestion'];
+          $titre = $c['titreQuestion'];
+        ?>
+        <div class="Question-forum">
+          <a href="http://www-etu-info.iut2.upmf-grenoble.fr/~claryd/Damienv4/Damienv3/Controler/commentaires.php?id=<?php echo urlencode($id);  ?>"><?php echo $titre; echo "    posté par : "; echo $pseudo; echo "      le "; echo $dateCom; ?></a>
+        </div>
+      <?php  } ?>
+    </div>
+  </body>
+</html>
